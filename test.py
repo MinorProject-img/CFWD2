34dcbeb424449c2b02b9aac3aa13c08c64826c4d
MinorProject-img <minorproject821@gmail.com>
2025-04-26 23:01:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -15,13 +15,13 @@ from tqdm import tqdm
 
 def parse_args_and_config():
     parser = argparse.ArgumentParser(description='Evaluate Wavelet-Based Diffusion Model')
-    parser.add_argument("--config", default='./configs/LOLv1.yml', type=str,
+    parser.add_argument("--config", default='LOLv1.yml', type=str,
                         help="Path to the config file")
     parser.add_argument('--resume', default='', type=str,
                         help='Path for the diffusion model checkpoint to load for evaluation')
     parser.add_argument("--sampling_timesteps", type=int, default=10,
                         help="Number of implicit sampling steps")
-    parser.add_argument("--image_folder", default='', type=str,
+    parser.add_argument("--image_folder", default='result/', type=str,
                         help="Location to save restored images")
     parser.add_argument('--seed', default=230, type=int, metavar='N',
                         help='Seed for initializing training (default: 230)')
